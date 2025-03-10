c55c62c0125cbab3405be3ffa0c5daae12155835
RaviRanjan20002 <raviranjankumar130082002@gmail.com>
2025-03-10 14:25:50 +0530

    updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,29 @@
+// /* eslint-disable no-undef */
+// const mongoose = require("mongoose");
+// require("dotenv").config();
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect("mongodb://localhost:27017/test_db", { 
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log("MongoDB Connected...");
+//   } catch (error) {
+//     console.error("MongoDB Connection Error:", error);
+//     process.exit(1); // Exit process if connection fails
+//   }
+// };
+
+// module.exports = connectDB;
+
 /* eslint-disable no-undef */
 const mongoose = require("mongoose");
 require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/test_db", { 
+    await mongoose.connect(process.env.MONGO_URI, { 
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
@@ -16,4 +35,3 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
-
