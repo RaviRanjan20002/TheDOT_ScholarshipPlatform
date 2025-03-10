1db2992b484957853b82201fa1362786c9998b1e
Nikita-Vaid <vaidnikita06@gmail.com>
2025-03-10 23:11:58 +0530

    Testsched

--- frontend/src/Component/Pages/TestSched.jsx
+++ frontend/src/Component/Pages/TestSched.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "../../Styles/TestSched.css"; // Import the CSS file
+import "../../Styles/Testsched.css"; // Import the CSS file
 import { useNavigate } from "react-router-dom";
 
 const TestSched = () => {
