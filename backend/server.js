71921ca2fb0149bce469d47f5cd2f889a033d4cd
RaviRanjan20002 <raviranjankumar130082002@gmail.com>
2025-03-11 09:23:56 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -11,10 +11,8 @@ connectDB();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.get("/" ).html("HI");
-
+// app.get("/" ).html("HI");
 app.use("/api/questions", questionRoutes);
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
